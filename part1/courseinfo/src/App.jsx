333cbdc181241369599e94b71b5d0fbbabcccaab
VEMIREDDY AMRUTHA REDDY <vemireddyamruthareddy75@gmail.com>
2026-01-24 14:04:18 +0530

    Complete Part 1 exercise 1.1
    Initial courseinfo application

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,48 @@
+const Header =(props) =>{
+  console.log(props)
+  return (
+    <>
+     <h1>COURSE : {props.course}</h1> 
+    </>
+  )
+}
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p> {props.title1} {props.ex1} </p>
+      <p> {props.title2} {props.ex2} </p>
+      <p> {props.title3} {props.ex3} </p>
+    </>
+  )
+}
+const Total = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p> Number of exercises = {props.Number} </p>
+    </>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content  title1={part1 } ex1={exercises1 } 
+        title2={part2} ex2={exercises2 }
+        title3={part3} ex3={exercises3} />
+      <Total Number ={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
+
